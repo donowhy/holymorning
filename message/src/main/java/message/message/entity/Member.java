84a5792df0f5e9f04e1c0fcbbfd32230fc6db90f
강민승 <k4minseung@gmail.com>
2023-07-25 01:20:12 +0900

    feat : Board

--- message/src/main/java/message/message/entity/Member.java
+++ message/src/main/java/message/message/entity/Member.java
@@ -6,13 +6,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class Member {
@@ -24,6 +25,7 @@ public class Member {
 	@Column(nullable = false, unique = true)
 	private String membername;
 
+	@JsonIgnore // 패스워드는 민감정보라 JSON으로 안만들어버린다!
 	@Column(nullable = false)
 	private String password;
 
